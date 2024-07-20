d7f17ffeeb6f9acc66053b7516ad70759e3b8504
Justin Vieira <justin.vieira@gmail.com>
2024-07-20 10:40:43 -0400

    Update target board/chips

--- aero_hydro_pump_saver_root_maximizer.c
+++ aero_hydro_pump_saver_root_maximizer.c
@@ -2,9 +2,8 @@
 
 aero_hydro_pump_saver_root_maximizer.c
 
-HARDWARE PLATFORM: arduino nano (https://store-usa.arduino.cc/products/arduino-nano/?selectedStore=us)
-or any other arduino compatible board. Note: you may need to change the OUTPUT_PIN
-below to match your board.
+HARDWARE PLATFORM: arduino & compatible boards, ATMEGA and compatible chips.
+Note: You may need to change the OUTPUT_PIN in code below to match your board.
 
 PURPOSE: Save $$$ by saving electricity and extending water/air pump life... While stimulating root growth.
 
@@ -21,18 +20,16 @@ Sensible defaults are provided for ON_INTERVAL and OFF_INTERVAL; However, interv
 should ideally be adjusted based on your pump, misters, and the size of your cloner.
 Best to experiment and observe the results.
 
+The arduino built-in LED is used to indicate when the pump is on. This can be useful for
+troubleshooting setup... Or debugging.
+
 There's a demo simulator here: https://wokwi.com/projects/403891674345275393
-Note: The demo has OUTPUT_PIN to LED_BUILTIN, which is the onboard LED on the arduino nano.
-When you run the code on an actual arduino nano, you will want OUTPUT_PIN to be the GPIO pin
-Connected to your relay that turns the water or air pump on and off... Wokwi/Simulator 
-just turns the onboard LED of the simulated arduino nano on and off during the on and off intervals.
 
 */
 
 #include <stdbool.h>
 
-// NOTE: OUTPUT_PIN can be set to LED_BUILTIN to blink the onboard LED
-const byte OUTPUT_PIN = 13;
+const byte OUTPUT_PIN = 12;
 
 unsigned long ON_INTERVAL = 10000;   // 10 seconds
 unsigned long OFF_INTERVAL = 120000; // 2 minutes
@@ -43,6 +40,7 @@ bool pinActive = false;
 void setup() {
   INTERVAL_TOTAL = ON_INTERVAL + OFF_INTERVAL;
   pinMode(OUTPUT_PIN, OUTPUT);      // Set OUTPUT_PIN to output mode
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
@@ -51,11 +49,13 @@ void loop() {
     if(!pinActive) {
       pinActive = true;
       digitalWrite(OUTPUT_PIN, HIGH);
+      digitalWrite(LED_BUILTIN, HIGH);
     }
   } else {
     if(pinActive) {
       pinActive = false;
       digitalWrite(OUTPUT_PIN, LOW);
+      digitalWrite(LED_BUILTIN, LOW);
     }
   }
 }
